7e0d65b08a0270078357d97eb23df7edfdc130d7
dmz <2421872737@qq.com>
2026-05-09 14:51:19 +0800

    feat:»Öhuifukecheng

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import home from './home/index'
 import wordBook from './word-book/index'
 import setting from './setting/index'
+import course from './course/index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
     ...home,
     ...wordBook,
     ...setting,
+    ...course,
   ]
 })
 
--- server/apps/server/src/app.module.ts
+++ server/apps/server/src/app.module.ts
@@ -8,8 +8,9 @@ import { WordBookModule } from './word-book/word-book.module';
  import {MinioModule} from '@libs/shared/minio/minio.module';
 import { TrackerModule } from './tracker/tracker.module';
 import { AuthModule } from './auth/auth.module';
+import { CourseModule } from './course/course.module';
 @Module({
-  imports: [UserModule, AuthModule, SharedModule, WordBookModule, TrackerModule,
+  imports: [UserModule, AuthModule, SharedModule, WordBookModule, TrackerModule, CourseModule,
      ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
